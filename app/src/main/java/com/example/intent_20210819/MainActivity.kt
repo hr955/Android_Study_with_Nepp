f12b20404bc278b2590700b11657685ee7586a7f
hr955 <nfzoze01@gmail.com>
2021-08-19 17:07:32 +0900

    임시 번호로 전화 걸기

--- app/src/main/java/com/example/intent_20210819/MainActivity.kt
+++ app/src/main/java/com/example/intent_20210819/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.intent_20210819
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -28,13 +29,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, EditNicknameActivity::class.java)
             startActivityForResult(intent, REQ_FOR_NICKNAME)
         }
+
+        btnDial.setOnClickListener {
+            val myUri = Uri.parse("tel:01012345678")
+            startActivity(Intent(Intent.ACTION_DIAL, myUri))
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == REQ_FOR_NICKNAME){
-            if(resultCode == Activity.RESULT_OK){
+        if (requestCode == REQ_FOR_NICKNAME) {
+            if (resultCode == Activity.RESULT_OK) {
                 tvNickname.text = data?.getStringExtra("nick")
             }
         }

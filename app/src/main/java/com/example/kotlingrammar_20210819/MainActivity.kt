93954427587941dac986a7f52a056c79131abc62
hr955 <nfzoze01@gmail.com>
2021-08-19 11:08:44 +0900

    변수 사용

--- app/src/main/java/com/example/kotlingrammar_20210819/MainActivity.kt
+++ app/src/main/java/com/example/kotlingrammar_20210819/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlingrammar_20210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //코틀린 문법 연습용 프로젝트 (UI 작업X)
+
+        val myName: String
+        myName = "장혜린"
+        //myName = "혜린" value는 변경 불가!!
+        Log.d("변수확인", myName)
+
+        var myBirthYear: Int
+        myBirthYear = 1995
+        Log.d("변수확인", myBirthYear.toString())
+
     }
 }

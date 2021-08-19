d2df58df1cf95eceaa8d03418eccd4020966191f
hr955 <nfzoze01@gmail.com>
2021-08-19 16:00:36 +0900

    메인화면 - 닉네임 + 변경버튼 배치

--- app/src/main/java/com/example/intent_20210819/ViewMessageActivity.kt
+++ app/src/main/java/com/example/intent_20210819/ViewMessageActivity.kt
@@ -1,6 +1,5 @@
 package com.example.intent_20210819
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_view_message.*

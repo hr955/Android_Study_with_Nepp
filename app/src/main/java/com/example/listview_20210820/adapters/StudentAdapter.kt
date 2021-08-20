6e44fcc5523609a57da68c52a1247fbc9de5ee5d
hr955 <nfzoze01@gmail.com>
2021-08-20 11:15:59 +0900

    Adapter 클래스 뼈대작업 마무리

--- app/src/main/java/com/example/listview_20210820/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_20210820/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listview_20210820.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_20210820.R
 import com.example.listview_20210820.datas.StudentData
 
 class StudentAdapter(
@@ -10,4 +14,16 @@ class StudentAdapter(
     private val mList: ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
 
+    // inflate : xml 을 변수에 담을 수 있도록 해줌
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = mInflater.inflate(R.layout.item_student_list, null)
+        }
+
+        return tempRow!!
+    }
 }

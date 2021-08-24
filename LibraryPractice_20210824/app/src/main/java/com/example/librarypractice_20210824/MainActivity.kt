7b59eb6f49369fa07bd9b9ae5ed67d2d7eda7368
hr955 <nfzoze01@gmail.com>
2021-08-24 16:43:10 +0900

    TedPermission Library / 권한 여부별 대응방법

--- LibraryPractice_20210824/app/src/main/java/com/example/librarypractice_20210824/MainActivity.kt
+++ LibraryPractice_20210824/app/src/main/java/com/example/librarypractice_20210824/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.librarypractice_20210824
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import com.example.librarypractice_20210824.base.BaseActivity
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -20,10 +22,24 @@ class MainActivity : BaseActivity() {
             startActivity(Intent(mContext, ViewPhotoActivity::class.java))
         }
 
-        callBtn.setOnClickListener {
-            val myUri = Uri.parse("tel:02-5555-8888")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+        //TedPermission 라이브러리 활용 -> 권한 여부 대응 + 실제 체크
+        val pl = object: PermissionListener{
+            
+            //권한이 있을 떄
+            override fun onPermissionGranted() {
+
+                // 전화연결 (CALL) 활용
+                callBtn.setOnClickListener {
+                    val myUri = Uri.parse("tel:02-5555-8888")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+            }
+
+            //권한이 없을 때
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(mContext, "권한이 필요합니다", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 

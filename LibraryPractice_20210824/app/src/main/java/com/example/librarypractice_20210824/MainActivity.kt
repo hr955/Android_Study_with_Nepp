7fd15366a1d73cc302ddddbd29e578787ed6ccdf
hr955 <nfzoze01@gmail.com>
2021-08-24 16:47:23 +0900

    CALL 권한 추가 (Menifests) 라이브러리 오류로 작업 불가

--- LibraryPractice_20210824/app/src/main/java/com/example/librarypractice_20210824/MainActivity.kt
+++ LibraryPractice_20210824/app/src/main/java/com/example/librarypractice_20210824/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.librarypractice_20210824.base.BaseActivity
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -18,13 +19,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-        profileImg.setOnClickListener {
-            startActivity(Intent(mContext, ViewPhotoActivity::class.java))
-        }
 
         //TedPermission 라이브러리 활용 -> 권한 여부 대응 + 실제 체크
         val pl = object: PermissionListener{
-            
+
             //권한이 있을 떄
             override fun onPermissionGranted() {
 
@@ -40,6 +38,13 @@ class MainActivity : BaseActivity() {
             override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
                 Toast.makeText(mContext, "권한이 필요합니다", Toast.LENGTH_SHORT).show()
             }
+
+            //실제 권한 확인 : Manifest에 작성 + 코틀린에서도 확인
+            //라이브러리 문제로 현재 작업 불가
+        }
+
+        profileImg.setOnClickListener {
+            startActivity(Intent(mContext, ViewPhotoActivity::class.java))
         }
     }
 

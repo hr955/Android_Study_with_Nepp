a0d0c8f6ad6ccca0b2c71346691bf1937dcbf1af
hr955 <nfzoze01@gmail.com>
2021-08-24 17:57:05 +0900

    앱 실행 2.5초 후 MainActivity로 이동

--- LibraryPractice_20210824/app/src/main/java/com/example/librarypractice_20210824/SplashActivity.kt
+++ LibraryPractice_20210824/app/src/main/java/com/example/librarypractice_20210824/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.librarypractice_20210824
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.librarypractice_20210824.base.BaseActivity
 
 class SplashActivity : BaseActivity() {
@@ -14,6 +17,10 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(mContext, MainActivity::class.java))
+            finish()
+        }, 2500)
     }
 
     override fun setValues() {

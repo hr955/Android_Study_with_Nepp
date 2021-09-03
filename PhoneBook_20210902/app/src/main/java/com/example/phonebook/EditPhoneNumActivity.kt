1b4abc4814b29648237281edefdb72f1753ecae0
hr955 <nfzoze01@gmail.com>
2021-09-03 09:42:29 +0900

    입력값 변수 생성

--- PhoneBook_20210902/app/src/main/java/com/example/phonebook/EditPhoneNumActivity.kt
+++ PhoneBook_20210902/app/src/main/java/com/example/phonebook/EditPhoneNumActivity.kt
@@ -25,8 +25,23 @@ class EditPhoneNumActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        okBtn.setOnClickListener {
+            // 1. 입력한 값들을 변수에 저장
+            val inputName = nameEdt.text.toString()
+            val inputPhoneNum = phoneNumEdt.toString()
+            //생년월일 -> yyyy-MM-dd 형태로 가공
+            val sdf = SimpleDateFormat("yyyy-MM-dd")
+            val birthDayStr = sdf.format(mSelectedDate.time)
+
+            // 2. 전화번호 데이터 객체로 만들기 (클래스 추가)
+
+            // 3. 전화번호를 "이름,전화번호,생년월일" 양식으로 가공 -> 파일에 저장
+        }
+
         selectBirthDayBtn.setOnClickListener {
             // 달력처럼, 날짜 선택 팝업 출현
+            // object 객체를 만듦
             val dateSetListener = object : DatePickerDialog.OnDateSetListener {
                 override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
                     // 날짜가 선택되면 실행해줄 코드 (년 / 월 / 일 세개의 Int를 가지고 처리)
@@ -38,9 +53,6 @@ class EditPhoneNumActivity : BaseActivity() {
 
                     val sdf = SimpleDateFormat("yyyy. MM. dd.")
                     birthDayTxt.text = sdf.format(mSelectedDate.time)
-
-
-
                 }
             }
 

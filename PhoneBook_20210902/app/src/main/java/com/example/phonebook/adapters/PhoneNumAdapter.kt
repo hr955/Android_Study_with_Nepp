e529a231475430c79aa156bce2a46104676d7ca0
hr955 <nfzoze01@gmail.com>
2021-09-03 10:56:17 +0900

    어댑터 뼈대작업

--- PhoneBook_20210902/app/src/main/java/com/example/phonebook/adapters/PhoneNumAdapter.kt
+++ PhoneBook_20210902/app/src/main/java/com/example/phonebook/adapters/PhoneNumAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.phonebook.adapters
 
-class PhoneNumAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.phonebook.R
+import com.example.phonebook.datas.PhoneNumData
+
+class PhoneNumAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<PhoneNumData>
+) :
+    ArrayAdapter<PhoneNumData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // getView : xml (우리가 만든 xml로 커스텀) -> position에 맞는 데이터를 입혀서 -> 리스트뷰의 각 position에 뿌려주기
+
+        var row = convertView
+        if(row == null){
+            row = mInflater.inflate(R.layout.phone_num_list_item, null)
+        }
+
+        row!! // row는 무조건 null 이 아니라고 강제
+
+        // 이 아래로는 row는 null이 아닌것으로 간주
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val birthDayTxt = row.findViewById<TextView>(R.id.birthDayTxt)
+
+        return row
+    }
 }

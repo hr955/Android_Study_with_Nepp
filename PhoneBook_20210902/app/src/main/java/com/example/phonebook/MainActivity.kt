5053a2b3c6cb22d1b7388f9e65989c97a280aacc
hr955 <nfzoze01@gmail.com>
2021-09-03 11:07:18 +0900

    리스트뷰 연결

--- PhoneBook_20210902/app/src/main/java/com/example/phonebook/MainActivity.kt
+++ PhoneBook_20210902/app/src/main/java/com/example/phonebook/MainActivity.kt
@@ -2,9 +2,18 @@ package com.example.phonebook
 
 import android.content.Intent
 import android.os.Bundle
+import com.example.phonebook.adapters.PhoneNumAdapter
+import com.example.phonebook.datas.PhoneNumData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    private val mPhoneNumList = ArrayList<PhoneNumData>()
+
+    // 1. 멤버변수로 빼는게 편한가?
+    // 2. 초기화 할 때 화면정보 (Context)가 필요한가? -> 필요하다 : onCreate 내부 or 이후에서 대입
+    // Hint : JAVA에서 배우는 클래스들 -> 대부분 화면 필요 X. val로 만들어도 된다
+    lateinit var mAdapter: PhoneNumAdapter
 
     // 0. 전화번호 추가 기능 -> 별도 화면에서 저장
 
@@ -23,7 +32,19 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-//        addPhoneNumBtn.setOnClickListener( object : View.OnClickListener {
+        mPhoneNumList.add(PhoneNumData("테스트", "010-1234-1234"))
+        mPhoneNumList.add(PhoneNumData("테스트1", "010-5678-5678"))
+        mPhoneNumList.add(PhoneNumData("테스트2", "010-2580-2580"))
+
+        // 어댑터 초기화
+        mAdapter = PhoneNumAdapter(mContext, R.layout.phone_num_list_item, mPhoneNumList)
+
+        // 리스트뷰의 어댑터로 연결
+        phoneNumListView.adapter = mAdapter
+    }
+
+    override fun setupEvents() {
+        //        addPhoneNumBtn.setOnClickListener( object : View.OnClickListener {
 //            override fun onClick(p0: View?) {
 //            }
 //        })
@@ -35,8 +56,4 @@ class MainActivity : BaseActivity() {
             startActivity(myIntent)
         }
     }
-
-    override fun setupEvents() {
-    }
-
 }
